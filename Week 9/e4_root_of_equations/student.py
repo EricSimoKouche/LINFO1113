838aaae5a4073ac7b5b6578d0ff6ce7f9f18d1ec
EricSimoKouche <eric.simokouche@student.uclouvain.be>
2026-04-13 09:05:12 +0200

    E4 Exercise

--- Week 9/e4_root_of_equations/student.py
+++ Week 9/e4_root_of_equations/student.py
@@ -14,6 +14,16 @@ def incremental_root_search(f, a, b, dx):
     # domain, it should return None.
 
     # TODO
+    i = 0
+    while (a + i * dx) < b - dx :
+        f_left = f(a + i * dx)
+        f_right = f(a + (i + 1) * dx)
+
+        # Making conditions this way avoid float underflow
+        if (f_left > 0) != (f_right > 0) or f_left == 0 or f_right == 0 :
+            return (a + i * dx, a + (i + 1) * dx)
+        
+        i += 1
     return None
 
 
@@ -27,7 +37,25 @@ def bisection_root_search(f, a, b, tol):
     # tolerance "tol" is reached.
 
     # TODO
-    return None
+    left, right = a, b
+
+    if f(left) == 0 : return left
+    if f(right) == 0 : return right
+
+    while abs(left - right) >= tol :
+        mid = (left + right) / 2.0
+
+        f_mid = f(mid)
+
+        if f_mid == 0:
+            return mid
+        
+        elif (f(left) < 0) != (f(mid) < 0) :
+            right = mid
+        else :
+            left = mid
+        
+    return (left + right) / 2.0
 
 
 def newton_raphson_root_search(f, df, a, b, tol):
@@ -42,7 +70,22 @@ def newton_raphson_root_search(f, df, a, b, tol):
     # that the algorithm diverges and return None.
 
     # TODO
-    return None
+    xi = (a + b) / 2.0
+
+    while True :
+
+        df_xi = df(xi)
+        if df_xi == 0 :
+            return None
+
+        xi1 = xi - (f(xi)/df(xi))
+        if xi1 < a or xi1 > b :
+            return None
+        
+        if abs(xi1 - xi) < tol :
+            return xi1
+
+        xi = xi1
 
 
 def improved_newton_raphson_root_search(f, df, a, b, tol):
@@ -58,4 +101,35 @@ def improved_newton_raphson_root_search(f, df, a, b, tol):
     # Return the estimated root once the tolerance "tol" is met.
 
     # TODO
-    return None
+    left, right = a, b
+    xi = (left + right) / 2
+    
+    while abs(right - left) >= tol :
+
+        df_xi = df(xi) 
+
+        if df_xi == 0 :
+            use_newton = False
+        else :
+            xi1 = xi - (f(xi) / df(xi))
+            use_newton = (left < xi1 < right)
+        
+        if use_newton :
+            next_x = xi1
+        else :
+            next_x = (left + right) / 2.0
+
+        if abs(next_x - xi) < tol :
+            return next_x
+        
+        xi = next_x
+        f_xi = f(xi) 
+        if f_xi == 0 :
+            return xi
+        
+        if (f(left) > 0) != (f_xi > 0) :
+            right = xi
+        else :
+            left = xi
+
+    return xi
